94a947c5c38e257c75df79b8ca1bc4a588a9ac80
Navneet Singh <navneet.singh@industrybuying.com>
2022-11-01 20:55:32 +0530

    added allowed host setting in prod env

--- backend/settings.py
+++ backend/settings.py
@@ -24,9 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m+(t)el*#t=zd*!5zojo%-h8=hbhle#rvhi=b#^hk@=e8qt+fw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get('MODE') == 'PROD':
+    DEBUG = False
+    ALLOWED_HOSTS = ['35.169.106.228']
+else:
+    DEBUG = True
+    ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
