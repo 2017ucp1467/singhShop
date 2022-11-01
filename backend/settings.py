5dabc66e0cd1ffce8f432611da59d6f6d3836d75
Navneet Singh <navneet.singh@industrybuying.com>
2022-11-01 22:01:08 +0530

    added admin user and email

--- backend/settings.py
+++ backend/settings.py
@@ -16,6 +16,9 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ADMINS = (
+    ('Navneet Kumar', 'singh.navneet2698@gmail.com'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
